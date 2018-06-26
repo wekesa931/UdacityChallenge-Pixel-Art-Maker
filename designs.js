2dc5b245ebedee78f9aa91e5572e29fe90f6d809
wekesa931 <wekesabill@gmail.com>
2018-06-26 21:02:55 +0300

    modification

--- designs.js
+++ designs.js
@@ -1,34 +1,34 @@
+//initialize the table ID and assign the row and cell elements to given values
+const tabular = '#pixelCanvas';
+const tabularWidth = '<tr></tr>';
+const tabularHeight =  '<td></td>';
 
-var table = '#pixelCanvas';
-var row;
-row = '<tr></tr>';
-var column;
-column =  '<td></td>';
-var color=$('#colorPicker');
 
 $('#sizePicker').submit(function makeGrid(event) {
  
    event.preventDefault();
+   //get the value of height input
    width = $('#inputWidth').val();
+   //get the value of the width input
   height = $('#inputHeight').val();
  
 for(let r=0;r<height;r++){
   
-  $(table).append(row);
+  $(tabular).append(tabularWidth);//adds table at the end depending on the input
  
    for(let s = 0;s<width;s++){
    
-     $('tr').last().append(column);
+     $('tr').last().append(tabularHeight);//adds rows depending on the input
    
  }
 }
 
 });
 
-$(table).on('click','td',function(){
-  let color = $('#colorPicker').val();
-  // Grab the input color each time, in case it's changed.
+$(tabular).on('click','td',function paintGrid(){         // Checks the input color from the color picker.
+  let tabularColor = $('#colorPicker').val();
+ 
 
-  $(this).css('background-color', color);
-  // Set this cell's background to the input color.
+  $(this).css('background-color', tabularColor);  // assigns the color to the selected cell
+  
 });
